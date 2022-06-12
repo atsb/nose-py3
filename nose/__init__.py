882d1622f370266180bf33ad76a1a374954f7d96
atsb <atsb@atsb>
2022-06-12 13:51:53 +0200

    Version bump
    Now on pip

--- nose/__init__.py
+++ nose/__init__.py
@@ -3,7 +3,7 @@ from nose.exc import SkipTest, DeprecatedTest
 from nose.tools import with_setup
 
 __author__ = 'Jason Pellerin'
-__versioninfo__ = (1, 4, 0)
+__versioninfo__ = (1, 4, 3)
 __version__ = '.'.join(map(str, __versioninfo__))
 
 __all__ = [
