atsb/nose-py3

commit bbd6d086b74acaa8fc739e75461bd13d2ec62dac

Fixes for pycharm (still not working) and other python3 fixes.

nose/plugins/logcapture.py
@@ -255,6 +255,5 @@ class LogCapture(Plugin):
         return map(safe_str, self.handler.buffer)
 
     def addCaptureToErr(self, ev, records):
-        return '\n'.join([safe_str(ev), ln('>> begin captured logging <<')] + \
-                         records + \
-                         [ln('>> end captured logging <<')])
+        return '\n'.join([safe_str(ev), ln('>> begin captured logging <<')]
+                         ).join(records).join([ln('>> end captured logging <<')])

nose/__init__.py
@@ -1,5 +1,5 @@
 from nose.core import collector, main, run, run_exit, runmodule
-# backwards compatibility
+from nose.exc import SkipTest, DeprecatedTest
 from nose.tools import with_setup
 
 __author__ = 'Jason Pellerin'
@@ -7,5 +7,6 @@ __versioninfo__ = (1, 3, 9)
 __version__ = '.'.join(map(str, __versioninfo__))
 
 __all__ = [
-    'main', 'run', 'run_exit', 'runmodule', 'with_setup', 'collector'
-]
+    'main', 'run', 'run_exit', 'runmodule', 'with_setup',
+    'SkipTest', 'DeprecatedTest', 'collector'
+    ]

nose/exc.py
@@ -5,3 +5,5 @@ versions of nose where skipped and deprecated tests were core
 functionality, rather than being provided by plugins. It may be
 removed in a future release.
 """
+from nose.plugins.skip import SkipTest
+from nose.plugins.deprecated import DeprecatedTest

nose/ext/dtcompat.py
@@ -1386,39 +1386,28 @@ class DocTestRunner:
                 failed.append(x)
         if verbose:
             if notests:
-                print
-                len(notests), "items had no tests:"
+                print(len(notests), "items had no tests:")
                 notests.sort()
                 for thing in notests:
-                    print
-                    "   ", thing
+                    print("   ", thing)
             if passed:
-                print
-                len(passed), "items passed all tests:"
+                print(len(passed), "items passed all tests:")
                 passed.sort()
                 for thing, count in passed:
-                    print
-                    " %3d tests in %s" % (count, thing)
+                    print(" %3d tests in %s" % (count, thing))
         if failed:
-            print
-            self.DIVIDER
-            print
-            len(failed), "items had failures:"
+            print(self.DIVIDER)
+            print(len(failed), "items had failures:")
             failed.sort()
             for thing, (f, t) in failed:
-                print
-                " %3d of %3d in %s" % (f, t, thing)
+                print(" %3d of %3d in %s" % (f, t, thing))
         if verbose:
-            print
-            totalt, "tests in", len(self._name2ft), "items."
-            print
-            totalt - totalf, "passed and", totalf, "failed."
+            print(totalt, "tests in", len(self._name2ft), "items.")
+            print(totalt - totalf, "passed and", totalf, "failed.")
         if totalf:
-            print
-            "***Test Failed***", totalf, "failures."
+            print("***Test Failed***", totalf, "failures.")
         elif verbose:
-            print
-            "Test passed."
+            print("Test passed.")
         return totalf, totalt
 
     # /////////////////////////////////////////////////////////////////
@@ -1428,9 +1417,8 @@ class DocTestRunner:
         d = self._name2ft
         for name, (f, t) in other._name2ft.items():
             if name in d:
-                print
-                "*** DocTestRunner.merge: '" + name + "' in both" \
-                                                      " testers; summing outcomes."
+                print("*** DocTestRunner.merge: '" + name + "' in both" \
+                                                            " testers; summing outcomes.")
                 f2, t2 = d[name]
                 f = f + f2
                 t = t + t2
@@ -1931,13 +1919,11 @@ class Tester:
     def runstring(self, s, name):
         test = DocTestParser().get_doctest(s, self.globs, name, None, None)
         if self.verbose:
-            print
-            "Running string", name
+            print("Running string", name)
         (f, t) = self.testrunner.run(test)
         if self.verbose:
-            print
-            f, "of", t, "examples failed in string", name
-        return (f, t)
+            print(f, "of", t, "examples failed in string", name)
+        return f, t
 
     def rundoc(self, object, name=None, module=None):
         f = t = 0
@@ -2316,8 +2302,7 @@ def debug_script(src, pm=False, globs=None):
             try:
                 exec(open(srcfilename, globs, globs).read())
             except:
-                print
-                sys.exc_info()[1]
+                print(sys.exc_info()[1])
                 pdb.post_mortem(sys.exc_info()[2])
         else:
             # Note that %r is vital here.  '%s' instead can, e.g., cause

nose/plugins/skip.py
@@ -7,23 +7,7 @@ is enabled by default but may be disabled with the ``--no-skip`` option.
 """
 
 from nose.plugins.errorclass import ErrorClass, ErrorClassPlugin
-
-# on SkipTest:
-#  - unittest SkipTest is first preference, but it's only available
-#    for >= 2.7
-#  - unittest2 SkipTest is second preference for older pythons.  This
-#    mirrors logic for choosing SkipTest exception in testtools
-#  - if none of the above, provide custom class
-try:
-    from unittest.case import SkipTest
-except ImportError:
-    try:
-        from unittest2.case import SkipTest
-    except ImportError:
-        class SkipTest(Exception):
-            """Raise this exception to mark a test as skipped.
-            """
-            pass
+from unittest.case import SkipTest
 
 
 class Skip(ErrorClassPlugin):

nose/proxy.py
@@ -155,12 +155,10 @@ class ResultProxy(object):
             self.shouldStop = True
 
     def addSkip(self, test, reason):
-        # 2.7 compat shim
         from nose.plugins.skip import SkipTest
         self.assertMyTest(test)
         plugins = self.plugins
         if not isinstance(reason, Exception):
-            # for Python 3.2+
             reason = Exception(reason)
         plugins.addError(self.test, (SkipTest, reason, None))
         self.result.addSkip(self.test, reason)

nose/result.py
@@ -11,7 +11,6 @@ reporting.
 import logging
 
 try:
-    # 2.7+
     from unittest.runner import _TextTestResult
 except ImportError:
     from unittest import _TextTestResult
@@ -46,7 +45,6 @@ class TextTestResult(_TextTestResult):
         _TextTestResult.__init__(self, stream, descriptions, verbosity)
 
     def addSkip(self, test, reason):
-        # 2.7 skip compat
         from nose.plugins.skip import SkipTest
         if SkipTest in self.errorClasses:
             storage, label, isfail = self.errorClasses[SkipTest]
@@ -62,12 +60,8 @@ class TextTestResult(_TextTestResult):
         try:
             exc_info = self._exc_info_to_string(err, test)
         except TypeError:
-            # 2.3 compat
             exc_info = self._exc_info_to_string(err)
         for cls, (storage, label, isfail) in self.errorClasses.items():
-            # if 'Skip' in cls.__name__ or 'Skip' in ec.__name__:
-            #    from nose.tools import set_trace
-            #    set_trace()
             if isclass(ec) and issubclass(ec, cls):
                 if isfail:
                     test.passed = False
@@ -144,7 +138,7 @@ class TextTestResult(_TextTestResult):
             write("OK")
         items = summary.items()
         if items:
-            items.sort()
+            sorted(items)
             write(" (")
             write(", ".join(["%s=%s" % (label, count) for
                              label, count in items]))
@@ -180,11 +174,9 @@ class TextTestResult(_TextTestResult):
             self.stream.write('E')
 
     def _exc_info_to_string(self, err, test=None):
-        # 2.7 skip compat
         from nose.plugins.skip import SkipTest
         if isclass(err[0]) and issubclass(err[0], SkipTest):
             return str(err[1])
-        # 2.3/2.4 -- 2.4 passes test, 2.3 does not
         try:
             return _TextTestResult._exc_info_to_string(self, err, test)
         except TypeError:

nosetests.1
@@ -1,6 +1,6 @@
 .\" Man page generated from reStructuredText.
 .
-.TH "NOSETESTS" "1" "February 6, 2020" "1.3.8" "nose"
+.TH "NOSETESTS" "1" "February 6, 2020" "1.3.9" "nose"
 .SH NAME
 nosetests \- Nicer testing for Python
 .

commit 959f57c3db92ed18ae35fa2801f87fb436a46535

Fix logcapture formatting
It was broken in bbd6d086b74acaa8fc739e75461bd13d2ec62dac. After that commit, it would apply str.join multiple times, each with the previous text as the separator. Since the final join only had one item, it would throw away all the text before it and only include ln('>> end captured logging <<') in the result.

Also, use a chaining iterator, rather than building up a new list as was done before bbd6d086b74acaa8fc739e75461bd13d2ec62dac.

nose/plugins/logcapture.py
@@ -15,6 +15,7 @@ You can remove other installed logging handlers with the
 ``--logging-clear-handlers`` option.
 """
 
+import itertools
 import logging
 import threading
 from logging import Handler
@@ -255,5 +256,9 @@ class LogCapture(Plugin):
         return map(safe_str, self.handler.buffer)
 
     def addCaptureToErr(self, ev, records):
-        return '\n'.join([safe_str(ev), ln('>> begin captured logging <<')]
-                         ).join(records).join([ln('>> end captured logging <<')])
+        return '\n'.join(itertools.chain(
+            [safe_str(ev), ln('>> begin captured logging <<')],
+            records,
+            [ln('>> end captured logging <<')],
+        ))  
+