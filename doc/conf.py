b25fbf899f6cb125c2ed67808f3ff105d7ee2bc9
atsb <44937323+atsb@users.noreply.github.com>
2023-06-03 16:27:27 +0200

    Update conf.py copyright years and names

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'nose'
 copyright = u'2009, Jason Pellerin'
-maintainer = u'2020, atsb'
+maintainer = u'2020 - 2023, Adam Bilbrough'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
